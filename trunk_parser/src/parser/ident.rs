a502c21a7f52ded9b24a34313d5700f3bb8d24e8
Ryan Chandler <ryangjchandler@gmail.com>
2022-07-28 00:48:31 +0100

    parser: ident method docs for clarity

--- trunk_parser/src/parser/ident.rs
+++ trunk_parser/src/parser/ident.rs
@@ -3,14 +3,17 @@ use trunk_lexer::TokenKind;
 use super::{ParseResult, ParseError};
 
 impl Parser {
+    /// Expect an unqualified identifier such as Foo or Bar.
     pub(crate) fn ident(&mut self) -> ParseResult<String> {
         Ok(expect!(self, TokenKind::Identifier(i), i, "expected identifier"))
     }
 
+    /// Expect an unqualified or qualified identifier such as Foo, Bar or Foo\Bar.
     pub(crate) fn name(&mut self) -> ParseResult<String> {
         Ok(expect!(self, TokenKind::Identifier(i) | TokenKind::QualifiedIdentifier(i), i, "expected identifier"))
     }
 
+    /// Expect an unqualified, qualified or fully qualified identifier such as Foo, Foo\Bar or \Foo\Bar. 
     pub(crate) fn full_name(&mut self) -> ParseResult<String> {
         Ok(expect!(self, TokenKind::Identifier(i) | TokenKind::QualifiedIdentifier(i) | TokenKind::FullyQualifiedIdentifier(i), i, "expected identifier"))
     }
