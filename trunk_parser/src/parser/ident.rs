dc4cb10ac3f425b82ca774c6df6e20d259751fe0
Ryan Chandler <ryangjchandler@gmail.com>
2022-08-09 11:56:23 +0100

    parser: add match as soft reserved keyword

--- trunk_parser/src/parser/ident.rs
+++ trunk_parser/src/parser/ident.rs
@@ -38,7 +38,7 @@ impl Parser {
             TokenKind::Static | TokenKind::Abstract | TokenKind::Final | TokenKind::For |
             TokenKind::Private | TokenKind::Protected | TokenKind::Public | TokenKind::Require |
             TokenKind::RequireOnce | TokenKind::New | TokenKind::Clone | TokenKind::If |
-            TokenKind::Else | TokenKind::ElseIf | TokenKind::Default | TokenKind::Enum => {
+            TokenKind::Else | TokenKind::ElseIf | TokenKind::Default | TokenKind::Enum | TokenKind::Match => {
                 let string = self.current.kind.to_string();
                 self.next();
                 Ok(string)
