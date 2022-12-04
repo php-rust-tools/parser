ab1cd39b8ebfc7f2e414cbfa413f89c82b786f5f
azjezz <azjezz@protonmail.com>
2022-12-04 13:12:48 +0100

    fix: formatting
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -212,12 +212,8 @@ impl Default for Token {
 impl Display for TokenKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let s = match self {
-            Self::StartHeredoc(label) => {
-                return write!(f, "<<<{}", label)
-            },
-            Self::EndHeredoc(label) => {
-                return write!(f, "{}", label)
-            },
+            Self::StartHeredoc(label) => return write!(f, "<<<{}", label),
+            Self::EndHeredoc(label) => return write!(f, "{}", label),
             Self::BangEquals => "!=",
             Self::From => "from",
             Self::Print => "print",
