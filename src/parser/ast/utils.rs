0846759bdae66e9892e31b1e7c7ea625a36778ee
Ryan Chandler <ryangjchandler@gmail.com>
2023-01-25 16:51:30 +0000

    parser: make CommaSeparated iterable

--- src/parser/ast/utils.rs
+++ src/parser/ast/utils.rs
@@ -1,3 +1,6 @@
+use std::slice::Iter;
+use std::slice::IterMut;
+
 use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
@@ -12,8 +15,27 @@ pub struct CommaSeparated<T> {
     pub commas: Vec<Span>, // `,`
 }
 
+impl<T> CommaSeparated<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        self.inner.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        self.inner.iter_mut()
+    }
+}
+
 impl<T: Node> Node for CommaSeparated<T> {
     fn children(&mut self) -> Vec<&mut dyn Node> {
         self.inner.iter_mut().map(|x| x as &mut dyn Node).collect()
+    }
+}
+
+impl<T> IntoIterator for CommaSeparated<T> {
+    type Item = T;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.inner.into_iter()
     }
 }
