01ad41ccdaff78a31f939defa3a2abfc610db063
Ryan Chandler <ryangjchandler@gmail.com>
2023-01-25 16:55:41 +0000

    parser: make AttributeGroup iterable

--- src/parser/ast/attributes.rs
+++ src/parser/ast/attributes.rs
@@ -1,3 +1,5 @@
+use std::slice::Iter;
+
 use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
@@ -21,4 +23,19 @@ pub struct AttributeGroup {
     pub start: Span,
     pub end: Span,
     pub members: Vec<Attribute>,
+}
+
+impl AttributeGroup {
+    pub fn iter(&self) -> Iter<'_, Attribute> {
+        self.members.iter()
+    }
+}
+
+impl IntoIterator for AttributeGroup {
+    type Item = Attribute;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.members.into_iter()
+    }
 }
