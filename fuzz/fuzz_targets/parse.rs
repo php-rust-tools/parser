14121508b4c90f2ddfd0545191c8421528f8ec07
Corey Farwell <coreyf@rwell.org>
2023-01-28 12:32:23 -0500

    Add basic parsing fuzz target.

---
+++ fuzz/fuzz_targets/parse.rs
@@ -0,0 +1,9 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    match php_parser_rs::parser::parse(data) {
+        Ok(ast) => {}
+        Err(err) = {}
+    }
+});
