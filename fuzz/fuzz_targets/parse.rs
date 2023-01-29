6d326aa5b2b1c539af32a9b63d4f2b8330f46f4e
Corey Farwell <coreyf@rwell.org>
2023-01-29 15:24:10 -0500

    Fix syntax

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     match php_parser_rs::parser::parse(data) {
         Ok(ast) => {}
-        Err(err) = {}
+        Err(err) => {}
     }
 });
