ac06e05059229007f28955c5291ad37fd019096c
Ryan Chandler <ryangjchandler@gmail.com>
2023-02-19 20:20:59 +0000

    parser: add left & right methods to assignment operation expression

--- src/parser/ast/operators.rs
+++ src/parser/ast/operators.rs
@@ -171,6 +171,46 @@ pub enum AssignmentOperationExpression {
     },
 }
 
+impl AssignmentOperationExpression {
+    pub fn left(&self) -> &Expression {
+        match self {
+            AssignmentOperationExpression::Assign { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Addition { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Subtraction { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Multiplication { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Division { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Modulo { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Exponentiation { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Concat { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::BitwiseAnd { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::BitwiseOr { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::BitwiseXor { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::LeftShift { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::RightShift { left, .. } => left.as_ref(),
+            AssignmentOperationExpression::Coalesce { left, .. } => left.as_ref(),
+        }
+    }
+
+    pub fn right(&self) -> &Expression {
+        match self {
+            AssignmentOperationExpression::Assign { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Addition { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Subtraction { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Multiplication { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Division { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Modulo { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Exponentiation { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Concat { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::BitwiseAnd { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::BitwiseOr { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::BitwiseXor { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::LeftShift { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::RightShift { right, .. } => right.as_ref(),
+            AssignmentOperationExpression::Coalesce { right, .. } => right.as_ref(),
+        }
+    }
+}
+
 impl Node for AssignmentOperationExpression {
     fn children(&mut self) -> Vec<&mut dyn Node> {
         match self {
